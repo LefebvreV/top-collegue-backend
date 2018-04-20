ae6bc3ae65a96e70182bef24a4743cad997a8cdc
Valentin <lefebvre.valentin76@gmail.com>
2018-04-20 17:37:14 +0200

    Ajout accès API Collegues

--- src/main/java/dev/collegue/controller/CollegueController.java
+++ src/main/java/dev/collegue/controller/CollegueController.java
@@ -1,8 +1,11 @@
 package dev.collegue.controller;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import dev.collegue.controller.vm.ActionIhm;
+import dev.collegue.controller.vm.CollegueJson;
+import dev.collegue.controller.vm.CollegueJsonApi;
 import dev.collegue.entite.Collegue;
 import dev.collegue.repository.CollegueRepository;
 import dev.collegue.service.CollegueService;
@@ -44,6 +50,34 @@ public class CollegueController {
 		}
 		colleguetRepo.save(update);
 		return update;
+	}
+
+
+	@RequestMapping(value = "/nouveau", method = RequestMethod.POST)
+	public void ajouter(@RequestBody CollegueJson collegue) {
+
+		final String uri = "http://collegues-api.cleverapps.io/collegues";
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<CollegueJsonApi[]> response = restTemplate.getForEntity(uri, CollegueJsonApi[].class);
+		List<CollegueJsonApi> list = Arrays.asList(response.getBody());
+		
+		Optional<CollegueJsonApi> collegueTrouve = list.stream().filter((CollegueJsonApi col) -> {
+			return col.getMatricule().equals(collegue.getMatricule());
+		}).findFirst();
+
+		collegueTrouve.ifPresent(c -> {
+			Collegue nouveau = new Collegue();
+			nouveau.setPseudo(collegue.getPseudo());
+			nouveau.setScore(0);
+			nouveau.setAdresse(c.getAdresse());
+			nouveau.setEmail(c.getEmail());
+			nouveau.setNom(c.getNom());
+			nouveau.setPrenom(c.getPrenom());
+			nouveau.setUrl(c.getPhoto());
+			colleguetRepo.save(nouveau);
+		});
+
 	}
 
 }
---
+++ src/main/java/dev/collegue/controller/vm/CollegueJson.java
@@ -0,0 +1,52 @@
+package dev.collegue.controller.vm;
+
+public class CollegueJson {
+	private String matricule;
+	private String pseudo;
+	private String url;
+
+	/**
+	 * @return the matricule
+	 */
+	public String getMatricule() {
+		return matricule;
+	}
+
+	/**
+	 * @param matricule
+	 *            the matricule to set
+	 */
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+
+	/**
+	 * @return the pseudo
+	 */
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	/**
+	 * @param pseudo
+	 *            the pseudo to set
+	 */
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * @param url
+	 *            the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
